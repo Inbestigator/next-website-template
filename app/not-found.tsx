6a50b0ad46b082201123033382da80c0ffdd6b4b
Inbestigator <inbestigatoryt@gmail.com>
2024-05-21 22:47:46 -0700

    1.5.0

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function NotFound() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center text-center">
+      <h1 className="text-3xl font-bold">404</h1>
+      <h2 className="text-sm">This page could not be found.</h2>
+    </div>
+  )
+}
