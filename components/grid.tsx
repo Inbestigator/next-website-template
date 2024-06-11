50c949afd8d0123dbf6fa3429ba0c9e61cacdec8
Inbestigator <119569726+Inbestigator@users.noreply.github.com>
2024-06-11 09:57:50 -0700

    Update grid.tsx

--- components/grid.tsx
+++ components/grid.tsx
@@ -6,7 +6,7 @@ export default function Grid() {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="hsl(var(--foreground))"
+      stroke="currentColor"
       strokeWidth="0.5"
     >
       <path d="M6 2v20M12 2v20M18 2v20M2 6h20M2 12h20M2 18h20" />
